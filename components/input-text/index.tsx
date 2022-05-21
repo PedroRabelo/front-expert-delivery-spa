a5b71ce06c11333ee5adf58ce19f92690ad56fba
pedro <pnarabelo@gmail.com>
2022-05-21 15:26:48 -0300

    feat: atualizado estilos do input

--- components/input-text/index.tsx
+++ components/input-text/index.tsx
@@ -1,4 +1,5 @@
 import { InputIcon } from "../input-icon";
+import Styles from "./styles.module.css";
 
 type Props = {
   label: string;
@@ -9,18 +10,16 @@ type Props = {
 export function InputText({ ...props }: Props) {
   return (
     <>
-      <div className="flex mb-14">
-        <div className="bg-indigo-100 w-16 mr-7">
+      <div className={Styles.container}>
+        <div className={Styles.icon}>
           <InputIcon size={24} color="#503E9D" />
         </div>
-        <div>
-          <label className="text-xs font-bold uppercase tracking-wider text-gray-800">
-            {props.label}
-          </label>
+        <div className="grow">
+          <label className={Styles.label}>{props.label}</label>
           <input
             type={props.type}
             placeholder={props.placeholder}
-            className="w-full py-3 border-b"
+            className={Styles.input}
           />
         </div>
       </div>
