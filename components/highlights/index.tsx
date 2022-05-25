085273376ac139aac9da451d7cf3b8439f13bf63
pedro <pnarabelo@gmail.com>
2022-05-24 23:21:27 -0300

    feat: ajustes highlights

--- components/highlights/index.tsx
+++ components/highlights/index.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+import { IconArrowRight } from "../icon-family";
+import Styles from "./styles.module.css";
+
 export const Highlights = () => {
   return (
-    <div className="bg-gray-500">
-      <p className="text-gray-100">Destaques</p>
+    <div>
+      <div className={Styles.header}>
+        <h2>Destaques</h2>
+        <Link href="/destaques">
+          <a>
+            Ver Todos
+            <IconArrowRight size={12} color="#5B4AA3" />
+          </a>
+        </Link>
+      </div>
+      <div>[Destaques]</div>
     </div>
   );
 };
